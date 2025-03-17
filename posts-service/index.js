6adf5b8eb9e18d24f63c33877d66423f3656bcd0
ratilla-voltaire <07402931@usc.edu.ph>
2025-03-17 08:21:04 +0800

    Continued working on it! pushing incase of broken!

---
+++ posts-service/index.js
@@ -0,0 +1,43 @@
+const { ApolloServer, gql } = require('apollo-server');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+const typeDefs = gql`
+  type Post {
+    id: ID!
+    title: String!
+    content: String!
+  }
+
+  type Query {
+    posts: [Post!]!
+    post(id: ID!): Post
+  }
+
+  type Mutation {
+    createPost(title: String!, content: String!): Post!
+    updatePost(id: ID!, title: String, content: String): Post!
+    deletePost(id: ID!): Post!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    posts: () => prisma.post.findMany(),
+    post: (_, args) => prisma.post.findUnique({ where: { id: parseInt(args.id) } }),
+  },
+  Mutation: {
+    createPost: (_, args) => prisma.post.create({ data: args }),
+    updatePost: (_, args) =>
+      prisma.post.update({ where: { id: parseInt(args.id) }, data: args }),
+    deletePost: (_, args) =>
+      prisma.post.delete({ where: { id: parseInt(args.id) } }),
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen({ port: 4002 }).then(({ url }) => {
+  console.log(`��� Posts service ready at ${url}`);
+});
---
+++ users-service/index.js
@@ -0,0 +1,43 @@
+const { ApolloServer, gql } = require('apollo-server');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+  }
+
+  type Query {
+    users: [User!]!
+    user(id: ID!): User
+  }
+
+  type Mutation {
+    createUser(name: String!, email: String!): User!
+    updateUser(id: ID!, name: String, email: String): User!
+    deleteUser(id: ID!): User!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    users: () => prisma.user.findMany(),
+    user: (_, args) => prisma.user.findUnique({ where: { id: parseInt(args.id) } }),
+  },
+  Mutation: {
+    createUser: (_, args) => prisma.user.create({ data: args }),
+    updateUser: (_, args) =>
+      prisma.user.update({ where: { id: parseInt(args.id) }, data: args }),
+    deleteUser: (_, args) =>
+      prisma.user.delete({ where: { id: parseInt(args.id) } }),
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen({ port: 4001 }).then(({ url }) => {
+  console.log(`íº€ Users service ready at ${url}`);
+});
