d6e6cb34105850302b47ca36c83e3fbdc0033e1f
ratilla-voltaire <07402931@usc.edu.ph>
2025-03-26 13:57:34 +0800

    It can fetch data but it was not using subscriptions

--- graphql-prisma-frontend/src/App.js
+++ graphql-prisma-frontend/src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { useQuery, useMutation } from '@apollo/client';
 import { GET_POSTS, CREATE_POST, POST_CREATED } from './graphql';
 import { useSubscription } from '@apollo/client';
+import './styles/PostsTable.css';
 
 function App() {
   const { loading, error, data } = useQuery(GET_POSTS);
@@ -22,19 +23,28 @@ function App() {
   };
 
   return (
-    <div>
+    <div className="table-container">
       <h1>Posts</h1>
-      <ul>
-        {data.posts.map((post) => (
-          <li key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.content}</p>
-          </li>
-        ))}
-      </ul>
-
+      <table className="posts-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Content</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.posts.map((post) => (
+            <tr key={post.id}>
+              <td>{post.id}</td>
+              <td>{post.title}</td>
+              <td>{post.content}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
